a7cc359b4644ebcbf0754cee3e971760d8b77f8f
tanpn <tan.devloper@gmail.com>
2025-10-28 22:46:39 +0700

    first commit

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
-# confirm current dir (optional)
 pwd
-
-# create the needed folders right here
-mkdir -p ./conf.d ./logs ./html
-
-# (optional) create empty files you’ll fill next
-touch ./nginx.conf ./docker-compose.yml ./conf.d/app.conf ./conf.d/api.conf
-
-# verify
+mkdir -p  ./logs
 ls -la
